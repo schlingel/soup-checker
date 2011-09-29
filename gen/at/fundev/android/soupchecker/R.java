2526ab7925b6afa3af02448c3c11b5d70eb94749
schlingel <der-schlingel@gmx.at>
2011-09-29 10:42:19 +0200

    Redesigned header - images are still missing

--- gen/at/fundev/android/soupchecker/R.java
+++ gen/at/fundev/android/soupchecker/R.java
@@ -18,14 +18,17 @@ public final class R {
         public static final int soup_user=0x7f020004;
     }
     public static final class id {
+        public static final int btnRefresh=0x7f070004;
+        public static final int btnSettings=0x7f070006;
+        public static final int hrLine=0x7f070007;
         public static final int imgVwUserLogo=0x7f070000;
         public static final int lblDate=0x7f070002;
         public static final int lblDescription=0x7f070001;
-        public static final int lblUserTxt=0x7f070004;
-        public static final int lvNotifications=0x7f070005;
-        public static final int refresh=0x7f070006;
+        public static final int lblUserTxt=0x7f070005;
+        public static final int lvNotifications=0x7f070008;
+        public static final int refresh=0x7f070009;
         public static final int rlHeader=0x7f070003;
-        public static final int settings=0x7f070007;
+        public static final int settings=0x7f07000a;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
